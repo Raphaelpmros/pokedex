3787fcc3d67c0c4377e0d6ec8ee30a55da4cc7b6
Raphael Mello <raphaelpmros@hotmail.com>
2024-01-25 18:02:34 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const next = document.querySelector('#buttonNext');
+const shiny = document.querySelector('#buttonShiny');
+const status = document.querySelector('#buttonStatus');
+const previous = document.querySelector('#buttonPrevious');
