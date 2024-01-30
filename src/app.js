e1a3efc5dbb073bd4832c9573d034a16f617de6c
Raphael Mello <raphaelpmros@hotmail.com>
2024-01-30 15:15:09 -0300

    funções iniciais finalizadas, dando inicio à tentativa de adicionar o botão de status de forma funcional

--- src/app.js
+++
@@ -1,6 +0,0 @@
-const next = document.querySelector("#buttonNext");
-const shiny = document.querySelector("#buttonShiny");
-const status = document.querySelector("#buttonStatus");
-const search = document.querySelector("#searchButton");
-const previous = document.querySelector("#buttonPrevious");
-
